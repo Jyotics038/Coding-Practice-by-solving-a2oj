27bee43fae6bab00a71a60e287b54f147d597112
jyoti <jyotibansal9466@gmail.com>
2021-08-05 17:12:27 +0530

    first question

---
+++ Div 2 A Practice/1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+    ll w;
+    cin>>w;
+    if(w==2){
+        cout<<"NO\n";
+    }
+    else if(w%2==0){
+        cout<<"YES\n";
+    }
+    else{
+        cout<<"NO\n";
+    }
+return 0;
+}
